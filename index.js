324d0d76e042953c094775a4c69def91a80fe83b
Hripsime Ghazaryan <94200499@mail.ru>
2021-12-28 15:24:08 +0400

    modified version of the code with array of objects

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-// function to generate random numbers from 0 to 100 
-const generateRandomNumber = () => Math.floor(Math.random() * 101);
-const listNumbers = [];
-let currentId = 0;
+const cardList = [];
 
-// template for each list item in the list
-const listItemTemplate = (numberGenerator) => {
-    let generatedNumber = numberGenerator();
-    listNumbers.push(generatedNumber);
+const generateRandomNumber = () => Math.floor(Math.random() * 100);
+
+const createListItem = () => {
+    let cardItem = {id: new Date().valueOf(), value: generateRandomNumber()};
+    cardList.push(cardItem);
+    addCards();
+}
+
+const listItemTemplate = (cardItem) => {
     return (
-        `<li class="card-space" id="${++currentId}">
+        `<li class="card-space" id="${cardItem.id}">
             <div class="card-number">
-                <h1 class="number">${generatedNumber}</h1>
+                <h1 class="number">${cardItem.value}</h1>
             </div>
             <div class="card-button">
-                <button class="button" onclick="removeCard(${currentId})">X</button>
+                <button class="button" onclick="removeCard()">X</button>
             </div>
         </li>`
     )
 }
 
-// function to add cards in the given number
-const addCards = (numberOfCards) => {
-    const cardList = document.querySelector(".card-list");
-    for(let i = 1; i <= numberOfCards; i++) {
-        cardList.innerHTML += listItemTemplate(generateRandomNumber);
+const addCards = () => {
+    const listItems = document.querySelector(".card-list");
+    listItems.innerHTML = "";
+    for(let i = 0; i < cardList.length; i++) {
+        listItems.innerHTML += listItemTemplate(cardList[i]);
     }
 }
 
-// sorting current numbers in the list
 const sortCards = () => {
-    const cardList = document.querySelectorAll(".number");
-    listNumbers.sort((a, b) => a - b);
-    for(let i = 0; i < listNumbers.length; i++) {
-        cardList[i].innerHTML = listNumbers[i];
+    const cardNumbers = document.querySelectorAll(".number");
+    cardList.sort((a, b) => a.value - b.value);
+    for(let i = 0; i < cardList.length; i++) {
+        cardNumbers[i].innerHTML = cardList[i].value;
     }
 }
 
-// remove card given it's id number 
-const removeCard = (id) => {
-    const listItem = document.getElementById(id);
+const removeCard = () => {
+    const parentId = event.target.parentNode.parentNode.id;
+    const listItem = document.getElementById(parentId);
     let numeberToBeRemoved = + listItem.firstElementChild.firstElementChild.innerHTML;
-    listNumbers.splice(listNumbers.indexOf(numeberToBeRemoved), 1);
+    cardList.splice(cardList.indexOf(numeberToBeRemoved), 1);
     listItem.parentNode.removeChild(listItem);
 }
-
-window.addEventListener("load", () => addCards(6));
